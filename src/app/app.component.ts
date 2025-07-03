621385ba182e6d26697c77acd764800568a08a46
leralys <valerialysko@gmail.com>
2025-07-03 17:36:38 +0300

    feat(tasks): added tasks component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,26 @@
 import { Component } from '@angular/core';
 
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
+import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
-
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = DUMMY_USERS[0].id;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
 
   onSelectUser(id: string) {
-    console.log('Selected user with id ' + id);
+    this.selectedUserId = id;
   }
 }
