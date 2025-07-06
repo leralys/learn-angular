96f464cdf602f9c4fb3e4838267d7c2278ca33ac
leralys <valerialysko@gmail.com>
2025-07-06 13:02:38 +0300

    feat(tasks): allow to remove tasks
    learn parent-child communication, changing state and evemt emitters

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
 
 @Component({
@@ -8,5 +8,17 @@ import { type Task } from './task.model';
   styleUrl: './task.component.css',
 })
 export class TaskComponent {
-  @Input({ required: true }) task!: Task; // this component expects to receive prop 'task'
+  @Input({ required: true }) task!: Task;
+  // This component *expects* to receive a 'task' object from its parent
+
+  // This is our "loudspeaker" named 'complete'
+  // The parent component can listen for this event: (complete)="onCompleteTask($event)" and trigger a method
+  // This event will send a string value
+  @Output() complete = new EventEmitter<string>();
+
+  // When this method is called (e.g., from a button click),
+  // we use the loudspeaker to tell the parent: "This task (by ID) is complete"
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
 }
