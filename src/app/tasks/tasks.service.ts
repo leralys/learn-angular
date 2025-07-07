3766396bfc1fdb008113d6981b146dcf4ea56cb6
leralys <valerialysko@gmail.com>
2025-07-07 06:41:45 +0300

    feat(tasks.service): persist tasks to the local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
 import { type NewTaskData } from './task/task.model';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class TasksService {
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   private tasks = [
     {
       id: 't1',
@@ -39,9 +47,15 @@ export class TasksService {
       userId,
       ...taskData,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
